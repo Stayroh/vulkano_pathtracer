53489b892b5d190d11ac454203ead8743719e4cf
stayroh <stayroh.official@gmail.com>
2025-12-28 01:15:02 +0100

    create swapchain and stuff before

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::{Context, Error, Result};
-use vulkano::{VulkanLibrary, instance::{Instance, InstanceExtensions}, swapchain::Surface};
+use vulkano::{VulkanLibrary, device::{Device, DeviceCreateInfo, DeviceExtensions, Queue, QueueCreateInfo, QueueFlags, physical::PhysicalDeviceType}, image::ImageUsage, instance::{Instance, InstanceExtensions}, swapchain::{Surface, Swapchain, SwapchainCreateInfo}};
 use std::default;
 use std::sync::Arc;
 use std::time::{Instant, Duration};
@@ -12,6 +12,10 @@ use winit::{
 
 struct GraphicsState {
     instance: Arc<Instance>,
+    window: Arc<Window>,
+    surface: Arc<Surface>,
+    device: Arc<Device>,
+    queue: Arc<Queue>,
 }
 
 impl GraphicsState {
@@ -28,8 +32,97 @@ impl GraphicsState {
         )
         .context("Failed to create Vulkan Instance")?;
 
+        let surface = Surface::from_window(instance.clone(), window.clone())
+            .context("Failed to create Surface from window")?;
+
+        let device_extensions = DeviceExtensions {
+            khr_swapchain: true,
+            khr_ray_tracing_pipeline: true,
+            khr_acceleration_structure: true,
+            khr_deferred_host_operations: true,
+            khr_buffer_device_address: true,
+            khr_spirv_1_4: true,
+            khr_shader_float_controls: true,
+            ..DeviceExtensions::empty()
+        };
+
+        let (physical_device, queue_family_index) = instance
+            .enumerate_physical_devices()
+            .context("Failed to enumerate physical devices")?
+            .filter(|p| p.supported_extensions().contains(&device_extensions))
+            .filter_map(|p| {
+                p.queue_family_properties()
+                    .iter()
+                    .enumerate()
+                    .position(|(i, q)| {
+                        q.queue_flags.intersects(QueueFlags::GRAPHICS | QueueFlags::COMPUTE)
+                            && p.surface_support(i as u32, &surface).unwrap_or(false)
+                    })
+                    .map(|q| (p, q as u32))
+            })
+            .min_by_key(|(p, _)| match p.properties().device_type {
+                PhysicalDeviceType::DiscreteGpu => 0,
+                PhysicalDeviceType::IntegratedGpu => 1,
+                PhysicalDeviceType::VirtualGpu => 2,
+                PhysicalDeviceType::Cpu => 3,
+                _ => 4,
+            })
+            .context("No suitable device found")?;
+
+        let (device, mut queues) = Device::new(
+            physical_device.clone(),
+            DeviceCreateInfo {
+                queue_create_infos: vec![QueueCreateInfo {
+                    queue_family_index,
+                    ..Default::default()
+                }],
+                enabled_extensions: device_extensions,
+                ..Default::default()
+            },
+        )
+        .context("Failed to create logical device")?;
+
+        let queue = queues.next().context("Failed to extract first queue out of queues")?;
+
+        let (mut swapchain, swapchain_images) = {
+            let caps = physical_device
+                .surface_capabilities(&surface, Default::default())
+                .context("Failed to get surface capabilities")?;
+
+            let dimensions = window.inner_size();
+
+            let composite_alpha = caps.supported_composite_alpha.into_iter().next().context("No supported composite alpha")?;
+            let image_format = physical_device
+                .surface_formats(&surface, Default::default())
+                .context("Failed to get surface formats")?
+                .get(0)
+                .context("No surface formats found")?
+                .0;
+            
+            Swapchain::new(
+                device.clone(),
+                surface.clone(),
+                SwapchainCreateInfo {
+                    min_image_count: caps.min_image_count,
+                    image_format,
+                    image_extent: dimensions.into(),
+                    image_usage: ImageUsage::COLOR_ATTACHMENT,
+                    composite_alpha,
+                    ..Default::default()
+                }
+            )
+            .context("Failed to create swapchain")?
+        };
+
+        
+        
+
         Ok(Self {
             instance,
+            window,
+            surface,
+            device,
+            queue,
         })
     }
 }
